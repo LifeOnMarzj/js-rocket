a77adb6fbacd060b40bb5e19c9c9a72053df2eaa
Life On Marzj <20445732+LifeOnMarzj@users.noreply.github.com>
2020-12-01 17:13:59 +0100

    Made my first rocket launch and made it look cool

---
+++ js/rocket.js
@@ -0,0 +1,47 @@
+var timer = null;
+var countdownNumber = 10;
+
+var changeState = function (state) {
+document.body.className = 'body-state' + state;
+clearInterval (timer);
+countdownNumber = 10;
+document.getElementById('countdown').innerHTML = countdownNumber;
+
+if (state == 2) {
+    timer = setInterval(function () {
+        countdownNumber = countdownNumber-1;
+        document.getElementById('countdown').innerHTML = countdownNumber;
+       
+        if (countdownNumber > 4 && countdownNumber <= 7) {
+            // be nervous
+            document.getElementById('nervous').className = 'nervous show';
+        } else {
+            document.getElementById('nervous').className = 'nervous';
+        }
+        if (countdownNumber > 1 && countdownNumber <= 4) {
+            // ready for planet Marzj
+            document.getElementById('marzj').className = 'marzj show';
+        } else{
+            document.getElementById('marzj').className = 'marzj';
+        }
+
+        if (countdownNumber <= 0) {
+            
+            changeState(3);
+        };
+    }, 700);
+    
+} else if (state == 3) {
+    var success = setTimeout(function ()
+    {
+var randomNumber = Math.round(Math.random()*10);
+console.log ('randomNumber:', randomNumber)
+//success
+if (randomNumber > 3){
+    changeState(4);
+} else {
+    changeState(5); //oh no!
+}
+    }, 2000); 
+};
+}
